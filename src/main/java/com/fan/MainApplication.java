d8e9a39888a98c4ad024356075fe6bcc15fdb990
coolerpluto <839035008@qq.com>
2021-11-25 20:26:05 +0800

    添加注释

--- src/main/java/com/fan/MainApplication.java
+++ src/main/java/com/fan/MainApplication.java
@@ -7,8 +7,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
-
+        //ConfigurableApplicationContext 是 IOC容器，里面有所有的组件
         ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class,args);
+        //getBeanDefinitionNames,从springboot容器里拿所有的组件的名称
         String[] beanDefinitionNames = run.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             System.out.println(beanDefinitionName);
